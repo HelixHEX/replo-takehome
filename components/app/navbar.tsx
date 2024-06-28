fe7a536d833448d9703b4ecef8fcaaf1d3272974
Elias Wambugu <e.wambugu192@gmail.com>
2024-06-28 11:02:16 -0700

    added user button to app navbar

--- components/app/navbar.tsx
+++ components/app/navbar.tsx
@@ -1,13 +1,15 @@
 "use client";
 
 import { DocumentContext } from "@/lib/context/document";
+import { UserButton } from "@clerk/nextjs";
 import { useContext } from "react";
 
 export default function AppNavbar() {
   const { document } = useContext(DocumentContext);
   return (
-    <div className="w-full h-10 p-2 pl-4">
+    <div className="w-full h-10 p-2 pl-4 flex justify-between">
       <p className="text-sm">{document?.title}</p>
+      <UserButton />
     </div>
   );
 }
