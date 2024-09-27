70a0080a6920b9c544ae02c2c6d2964ef990c6b2
Elias Wambugu <e.wambugu192@gmail.com>
2024-09-27 16:28:25 -0700

    fixed missing navbar prop in auth pages

--- app/sign-in/[[...sign-in]]/page.tsx
+++ app/sign-in/[[...sign-in]]/page.tsx
@@ -1,13 +1,31 @@
+"use client";
 import Navbar from "@/components/navbar";
 import { SignIn } from "@clerk/nextjs";
+import { useEffect, useState } from "react";
 
 export default function Page() {
-  return (
-    <>
-      <Navbar />
-      <div className=" w-full h-screen flex items-center justify-center">
-        <SignIn />
-      </div>
-    </>
-  );
+	const [scrollY, setScrollY] = useState(0);
+
+	useEffect(() => {
+		const onScroll = () => {
+			console.log(window.scrollY);
+			setScrollY(window.scrollY);
+		};
+
+		onScroll();
+
+		window.addEventListener("scroll", onScroll);
+
+		return () => {
+			window.removeEventListener("scroll", onScroll);
+		};
+	}, []);
+	return (
+		<>
+			<Navbar scrollTop={scrollY} />
+			<div className=" w-full h-screen flex items-center justify-center">
+				<SignIn />
+			</div>
+		</>
+	);
 }
