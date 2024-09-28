13cbe63bee6343047a35c6168cf89b10200d938d
Elias Wambugu <e.wambugu192@gmail.com>
2024-09-28 10:17:37 -0700

    updated utils file for navbar component

--- lib/utils.ts
+++ lib/utils.ts
@@ -1,6 +1,25 @@
-import { type ClassValue, clsx } from "clsx"
-import { twMerge } from "tailwind-merge"
+import { type ClassValue, clsx } from "clsx";
+import { SyntheticEvent, useRef, useState } from "react";
+import { twMerge } from "tailwind-merge";
 
 export function cn(...inputs: ClassValue[]) {
-  return twMerge(clsx(inputs))
+  return twMerge(clsx(inputs));
 }
+
+export const useScrollTop = (): {
+  ref: React.RefObject<HTMLDivElement>;
+  onScroll: () => void;
+  scrollTop: number;
+} => {
+  const ref = useRef<HTMLDivElement>(null);
+  const [scrollTop, setScrollTop] = useState(0);
+
+  const onScroll = () => {
+    console.log("onScroll");
+    if (ref.current) {
+      setScrollTop(ref.current.scrollTop);
+    }
+  };
+
+  return { ref, onScroll, scrollTop };
+};
