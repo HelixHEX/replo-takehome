1a1f521f90a21bbeaac2dea4414a405ffc3314f0
Elias Wambugu <e.wambugu192@gmail.com>
2024-09-28 14:10:28 -0700

    updated document title styling

--- components/documentName.tsx
+++ components/documentName.tsx
@@ -6,31 +6,20 @@ import { Input } from "./ui/input";
 import { useUpdateDocumentTitle } from "@/lib/api/mutations";
 
 export default function DocumentName() {
-  const { document } = useContext(DocumentContext);
-  const [mode, setMode] = useState<"text" | "edit">("text");
-  const [title, setTitle] = useState<string>(document?.title || "");
-  const { mutate } = useUpdateDocumentTitle(document?.id);
-  return (
-    <>
-      {mode === "text" ? (
-        <h1
-          onClick={() => setMode("edit")}
-          className="text-3xl font-bold text-gray-800"
-        >
-          {title}
-        </h1>
-      ) : (
-        <Input
-          placeholder="Document title..."
-          className="text-3xl font-bold text-gray-800"
-          onChange={(e) => {
-            mutate({ id: document?.id, title: e.target.value });
-            setTitle(e.target.value);
-          }}
-          value={title}
-          onMouseLeave={() => setMode("text")}
-        />
-      )}
-    </>
-  );
+	const { document } = useContext(DocumentContext);
+	const [title, setTitle] = useState<string>(document?.title || "");
+	const { mutate } = useUpdateDocumentTitle(document?.id);
+	return (
+		<>
+			<Input
+				placeholder="Document title..."
+				className="text-3xl bg-[#fcfcfc] focus:border-none hover:bg-none  p-2 border-none font-bold text-gray-800"
+				onChange={(e) => {
+					mutate({ id: document?.id, title: e.target.value });
+					setTitle(e.target.value);
+				}}
+				value={title}
+			/>
+		</>
+	);
 }
